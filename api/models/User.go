c55e3fce92f099e9a373157b1b8feb393424250a
Hamid Teimouri <hamidteimouri@outlook.com>
2020-10-11 14:49:40 +0330

    changed

--- api/models/User.go
+++ api/models/User.go
@@ -15,8 +15,8 @@ import (
 type User struct {
 	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
 	Nickname  string    `gorm:"size:255;not null;unique" json:"nickname"`
-	Email     string    `gorm:"size:100;not null;unique" json:"email"`
-	Password  string    `gorm:"size:100;not null;" json:"password"`
+	Email     string    `gorm:"size:128;not null;unique" json:"email"`
+	Password  string    `gorm:"size:128;not null;" json:"password"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
--- api/server.go
+++ api/server.go
@@ -21,8 +21,12 @@ func Run() {
 		fmt.Println("getting the env values")
 	}
 
-	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+	server.Initialize(os.Getenv("DB_DRIVER"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_NAME"))
 
 	//seed.Load(server.DB)
 
